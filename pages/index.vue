8396b30d3ef8866b9eac2f9687862a3f107e4226
Nkeze Sylvester <silverman42>
2021-09-23 16:26:35 +0100

    First push

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- Hero phrase start -->
+  </div>
+</template>
+
+<script>
+import observer from '~/assets/mixins/Observer'
+export default {
+  mixins: [observer],
+  layout: 'Main',
+  transition: {
+    name: 'zoom',
+    mode: 'out-in',
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style>
+.nuxt-content ul li {
+  @apply mb-5 relative pl-5;
+}
+.nuxt-content ul li::before {
+  content: '▹';
+  @apply text-primary-400 top-0 left-0 absolute text-base;
+}
+
+.nuxt-content a {
+  @apply text-primary-600 hover:text-primary-900;
+}
+</style>
